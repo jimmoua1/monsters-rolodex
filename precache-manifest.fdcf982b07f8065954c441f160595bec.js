1b83dda7f5245f73ec973008de128d0c9bdb8564
jimmoua1 <tsheejmaus@yahoo.com>
2020-07-01 00:34:13 -0700

    Updates

---
+++ precache-manifest.fdcf982b07f8065954c441f160595bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb099c07abdd6fa7517df1a98f552f19",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f07be81138f7aa084a6",
+    "url": "/monsters-rolodex/static/css/main.90badaeb.chunk.css"
+  },
+  {
+    "revision": "e77b145a50d07c4351f3",
+    "url": "/monsters-rolodex/static/js/2.cf976da0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.cf976da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f07be81138f7aa084a6",
+    "url": "/monsters-rolodex/static/js/main.fa972bf7.chunk.js"
+  },
+  {
+    "revision": "ed90dde62184553a0c6e",
+    "url": "/monsters-rolodex/static/js/runtime-main.747c856a.js"
+  }
+]);
